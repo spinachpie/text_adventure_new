822e9fc349533d2ccd6e1b8cff1b094b54cdee8b
Erik Bok <eriktbok@gmail.com>
2020-08-21 23:33:37 +0000

    hunger meter (still need food and death), some upstairs rooms and item changes

--- globals.py
+++ globals.py
@@ -7,5 +7,21 @@ def IntroText(context):
 #  like the player's starting location.
 def InitialSetup(context):
     context.player.SetPlayerLocation("OUTSIDE_MANOR")
+    context.player.hunger_level = 0
     context.actions.swear_words = {"SHIT", "DAMN", "FUCK"}
     context.actions.swear_response = "A bird dropping hits you in the face for swearing!"
+    context.events.CreateEventInNMoves(CheckHunger, 0)
+
+def CheckHunger(context):
+  context.events.CreateEventInNMoves(CheckHunger, 1)
+  context.player.hunger_level += 1
+  if context.player.hunger_level == 101:
+    context.Print("You're starting to feel a little hungry.")
+  if context.player.hunger_level == 130:
+    context.Print("You are getting pretty hungry. It might be time to find a sandwich.")
+  if context.player.hunger_level == 165:
+    context.Print("If you don't eat soon, you will probably die. Yeah, I know it's extreme.")
+  if context.player.hunger_level == 190:
+    context.Print("Gandhi survived without food for 21 days. You're not Gandhi. You're about to starve.")
+  if context.player.hunger_level == 200:
+    context.Print("You die from hunger. Better luck next time.")
